30337a029fa622d2263b48f4ec4dcf5b1b589418
roll <roll@respect31.com>
2015-11-20 18:21:46 +0300

    improved activate

--- activate.sh
+++ activate.sh
@@ -1,11 +1,15 @@
-# Activation
+# Node
 nvm install 4
 nvm use 4
 npm install
+
+# Python
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pip install -r requirements.dev.txt
+
+# Git commit hook
 if [ ! -f .git/hooks/pre-commit ]; then
     echo -e "#!/bin/sh\n\n\nnpm run check" > .git/hooks/pre-commit
     chmod +x .git/hooks/pre-commit
