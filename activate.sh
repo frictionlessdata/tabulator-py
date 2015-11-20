bca7b1ff4b2a9378f1ab59f78fcebf0b2e1907eb
roll <roll@respect31.com>
2015-11-20 18:17:29 +0300

    fixes

--- activate.sh
+++ activate.sh
@@ -5,4 +5,6 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pip install -r requirements.dev.txt
-echo -e "#!/bin/sh\n\n\nnpm run check" > .git/hooks/pre-commit
+if [ ! -f .git/hooks/pre-commit ]; then
+    echo -e "#!/bin/sh\n\n\nnpm run check" > .git/hooks/pre-commit
+fi
