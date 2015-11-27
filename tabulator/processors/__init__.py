b70c7f93cf0ff4f988d1823cb5228f700cead8ff
roll <roll@respect31.com>
2015-11-27 15:59:30 +0300

    added python2 boilerplate code

--- tabulator/processors/__init__.py
+++ tabulator/processors/__init__.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
 from .api import API
 from .headers import Headers
 from .schema import Schema
--- tabulator/processors/schema.py
+++ tabulator/processors/schema.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from .api import API
 
 
-# TODO: implement
 class Schema(API):
 
     # Public
