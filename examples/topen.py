729c4d06a8c9650511efe37eb3c6b21a20189e6f
roll <roll@respect31.com>
2015-11-27 23:28:39 +0300

    updated example

--- examples/topen.py
+++ examples/topen.py
@@ -1,7 +1,7 @@
 from tabulator import topen, loaders, parsers, processors
 
 
-with topen('text://id,name\n1,Joe\n2,Rachel\n', format='csv') as table:
+with topen('text://id,name\n1,name1\n2,name2\n', format='csv') as table:
     table.add_processor(processors.Headers())
     for row in table.readrow(with_headers=True):
         print(row)
@@ -18,7 +18,8 @@ with topen('examples/data/table.xls') as table:
         print(row)
 
 
-source = 'https://raw.githubusercontent.com/okfn/tabulator-py/master/examples/data/table.csv'
+source = 'https://raw.githubusercontent.com'
+source += '/okfn/tabulator-py/master/examples/data/table.csv'
 with topen(source) as table:
     table.add_processor(processors.Headers())
     for row in table.readrow(with_headers=True):
