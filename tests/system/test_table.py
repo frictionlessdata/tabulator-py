c1109e18bee15eaf330af17f06f7397eb7349f74
roll <roll@respect31.com>
2015-11-27 13:27:45 +0300

    added stub tests

---
+++ tests/system/test_table.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import Mock, patch
+from tabulator import Table
+
+
+class TableTest(unittest.TestCase):
+
+    # Tests
+
+    def test(self):
+        self.assertTrue(True)
---
+++ tests/system/test_topen.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import Mock, patch
+from tabulator import topen
+
+
+class topenTest(unittest.TestCase):
+
+    # Tests
+
+    def test(self):
+        self.assertTrue(True)
