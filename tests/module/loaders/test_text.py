286bd10dd5f72f4d5d5f242ff22f56c9d157ceb6
roll <roll@respect31.com>
2015-11-29 10:06:56 +0300

    added python2 boilerplate to tests

--- tests/module/loaders/test_text.py
+++ tests/module/loaders/test_text.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import unittest
 from importlib import import_module
 module = import_module('tabulator.loaders.text')
