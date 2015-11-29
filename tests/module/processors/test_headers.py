5fa2889fba6c5f011e740f66d4df9d5ca443700c
roll <roll@respect31.com>
2015-11-29 10:09:08 +0300

    added processors test stubs

---
+++ tests/module/processors/test_headers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import unittest
+from importlib import import_module
+module = import_module('tabulator.processors.headers')
+
+
+class HeadersTest(unittest.TestCase):
+
+    # Tests
+
+    def test(self):
+        self.assertTrue(module.Headers)
---
+++ tests/module/processors/test_types.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import unittest
+from importlib import import_module
+module = import_module('tabulator.processors.types')
+
+
+class TypesTest(unittest.TestCase):
+
+    # Tests
+
+    def test(self):
+        self.assertTrue(module.Types)
