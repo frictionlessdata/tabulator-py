79ce8516a6ebad77a949806f3832458fed11017d
roll <roll@respect31.com>
2015-11-30 03:34:01 +0300

    implemented loaders.Text tests

--- tests/module/loaders/test_text.py
+++ tests/module/loaders/test_text.py
@@ -11,7 +11,22 @@ module = import_module('tabulator.loaders.text')
 
 class TextTest(unittest.TestCase):
 
+    # Actions
+
+    def setUp(self):
+        self.source = 'id,name\n1,name1\n2,name2\n'
+        self.encoding = 'utf-8'
+        self.loader = module.Text(self.source, self.encoding)
+
     # Tests
 
-    def test(self):
-        self.assertTrue(module.Text)
+    def test_load_t(self):
+        chars = self.loader.load(mode='t')
+        self.assertEqual(chars.read(), 'id,name\n1,name1\n2,name2\n')
+
+    def test_load_b(self):
+        chars = self.loader.load(mode='b')
+        self.assertEqual(chars.read(), b'id,name\n1,name1\n2,name2\n')
+
+    def test_encoding(self):
+        self.assertEqual(self.loader.encoding, self.encoding)
