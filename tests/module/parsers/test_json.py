a06ef98c8cf14e1de553f3fea00e001a274310d7
roll <roll@respect31.com>
2015-11-30 04:03:56 +0300

    implemented parsers.JSON test

--- tests/module/parsers/test_json.py
+++ tests/module/parsers/test_json.py
@@ -4,14 +4,43 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import os
 import unittest
+try:
+    from unittest.mock import Mock
+except ImportError:
+    from mock import Mock
 from importlib import import_module
 module = import_module('tabulator.parsers.json')
 
 
 class JSONTest(unittest.TestCase):
 
+    # Actions
+
+    def setUp(self):
+        basedir = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+        self.source = os.path.join(basedir, 'examples', 'data', 'table-dicts.json')
+        self.loader = Mock()
+        self.loader.load = Mock(return_value=open(self.source))
+        self.parser = module.JSON()
+
     # Tests
 
     def test(self):
-        self.assertTrue(module.JSON)
+
+        self.assertTrue(self.parser.closed)
+        self.parser.open(self.loader)
+        self.assertFalse(self.parser.closed)
+
+        self.assertEqual(
+            list(self.parser.items),
+            [(('id', 'name'), (1, 'name1')),
+             (('id', 'name'), (2, 'name2'))])
+
+        self.assertEqual(len(list(self.parser.items)), 0)
+        self.parser.reset()
+        self.assertEqual(len(list(self.parser.items)), 2)
+
+        self.parser.close()
+        self.assertTrue(self.parser.closed)
