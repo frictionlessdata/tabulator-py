70466fcf016377ac6174f8d492bfb4df9e391012
roll <roll@respect31.com>
2015-11-30 08:24:38 +0300

    implemented topen test

--- tests/module/test_topen.py
+++ tests/module/test_topen.py
@@ -5,13 +5,61 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import unittest
+try:
+    from unittest.mock import Mock, patch
+except ImportError:
+    from mock import Mock, patch
 from importlib import import_module
 module = import_module('tabulator.topen')
 
 
 class topenTest(unittest.TestCase):
 
+    # Actions
+
+    def setUp(self):
+        self.addCleanup(patch.stopall)
+        self.Table = patch.object(module, 'Table').start()
+        self.Loader = Mock()
+        self.Parser = Mock()
+        self.encoding = 'encoding'
+
     # Tests
 
-    def test(self):
-        self.assertTrue(module.topen)
+    def test_supported_in_source(self):
+        for scheme in ['file', 'text', 'ftp', 'ftps', 'http', 'https']:
+            for format in ['csv', 'xls', 'xlsx', 'json']:
+                patch.object(module, 'LOADERS', {scheme: self.Loader}).start()
+                patch.object(module, 'PARSERS', {format: self.Parser}).start()
+                source = '%s://path.%s' % (scheme, format)
+                table = module.topen(source, encoding=self.encoding)
+                self.assertEqual(table, self.Table.return_value)
+                self.Loader.assert_calles_with(source, self.encoding)
+                self.Parser.assert_calles_with()
+                self.Table.assert_called_with(
+                        loader=self.Loader(),
+                        parser=self.Parser())
+
+    def test_supported_in_parameters(self):
+        for scheme in ['file', 'text', 'ftp', 'ftps', 'http', 'https']:
+            for format in ['csv', 'xls', 'xlsx', 'json']:
+                patch.object(module, 'LOADERS', {scheme: self.Loader}).start()
+                patch.object(module, 'PARSERS', {format: self.Parser}).start()
+                source = 'path'
+                table = module.topen(
+                        source,
+                        scheme=scheme,
+                        format=format,
+                        encoding=self.encoding)
+                self.assertEqual(table, self.Table.return_value)
+                self.Loader.assert_calles_with(source, self.encoding)
+                self.Parser.assert_calles_with()
+                self.Table.assert_called_with(
+                        loader=self.Loader(),
+                        parser=self.Parser())
+
+    def test_not_supported(self):
+        self.assertRaises(Exception, module.topen, 'path',
+                scheme='not_supported', format='csv')
+        self.assertRaises(Exception, module.topen, 'path',
+                scheme='file', format='not_supported')
