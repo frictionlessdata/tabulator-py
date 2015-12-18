f07a2132083cc27a2ad18f8a0c41f20f3180651d
roll <roll@post.agency>
2015-12-18 14:24:00 +0300

    [#20] added parsers.Excelx test

---
+++ tests/module/parsers/test_excelx.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+import os
+import unittest
+from mock import Mock
+from importlib import import_module
+module = import_module('tabulator.parsers.excelx')
+
+
+class ExcelxTest(unittest.TestCase):
+
+    # Actions
+
+    def setUp(self):
+        basedir = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+        self.source = os.path.join(basedir, 'examples', 'data', 'table.xlsx')
+        self.loader = Mock()
+        self.loader.load = Mock(return_value=io.open(self.source, 'rb'))
+        self.parser = module.Excelx()
+
+    # Tests
+
+    def test(self):
+
+        self.assertTrue(self.parser.closed)
+        self.parser.open(self.loader)
+        self.assertFalse(self.parser.closed)
+
+        self.assertEqual(
+            list(self.parser.items),
+            [(None, ('id', 'name')),
+                (None, (1.0, 'english')),
+                (None, (2.0, '中国人'))])
+
+        self.assertEqual(len(list(self.parser.items)), 0)
+        self.parser.reset()
+        self.assertEqual(len(list(self.parser.items)), 3)
+
+        self.parser.close()
+        self.assertTrue(self.parser.closed)
