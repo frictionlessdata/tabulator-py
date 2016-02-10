26ce7a53a6a613bbec989e406fa0633427b18708
roll <roll@post.agency>
2016-02-10 11:10:15 +0300

    merged metadata into setup.py

--- Makefile
+++ Makefile
@@ -9,8 +9,7 @@ list:
 	@$(MAKE) -pRrq -f $(lastword $(MAKEFILE_LIST)) : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | xargs
 
 install:
-	pip install --upgrade -e .
-	pip install --upgrade -r tests_require
+	pip install --upgrade -e . [development]
 
 review:
 	pylint $(PKG)
--- setup.py
+++ setup.py
@@ -14,33 +14,64 @@ from setuptools import setup, find_packages
 def read(path):
     basedir = os.path.dirname(__file__)
     return io.open(os.path.join(basedir, path), encoding='utf-8').read()
+def version(name):
+    return '0.3.2' # implement
 
 
 # Prepare
-readme = read('README.md')
-license = read('LICENSE.md')
-requirements = read('requirements.txt').split()
-requirements_dev = read('requirements.dev.txt').split()
-package = json.loads(read('package.json'))
+NAME = 'tabulator'
+README = read('README.md')
+LICENSE = read('LICENSE.md')
+VERSION = version(NAME)
+PACKAGES = find_packages(exclude=['examples', 'tests'])
+INSTALL_REQUIRES = [
+    'six',
+    'xlrd',
+    'ijson',
+    'chardet',
+    'openpyxl',
+    'jsontableschema',
+]
+TESTS_REQUIRE = [
+    'pylint',
+    'tox',
+    'mock',
+    'pytest',
+    'pytest-cov',
+    'coverage',
+    'coveralls',
+]
 
 
 # Run
 setup(
-    name=package['name'],
-    version=package['version'],
-    description=package['description'],
-    long_description=readme,
-    author=package['author'],
-    author_email=package['author_email'],
-    url=package['repository'],
-    license=package['license'],
+    name='tabulator',
+    version=VERSION,
+    packages=PACKAGES,
     include_package_data=True,
-    packages=find_packages(exclude=['examples', 'tests']),
-    package_dir={package['slug']: package['slug']},
-    install_requires=requirements,
-    tests_require=requirements_dev,
-    test_suite='nose.collector',
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require = {'development': TESTS_REQUIRE},
+    test_suite='make test',
     zip_safe=False,
-    keywords=package['keywords'],
-    classifiers=package['classifiers'],
+    description='A utility library that provides a consistent interface for reading tabular data.',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='https://github.com/okfn/tabulator-py',
+    license=LICENSE,
+    keywords="frictionless data",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
