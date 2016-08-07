edca56592d3d4edce87c822c5d22d1c7f2b84f01
roll <roll@post.agency>
2016-08-07 21:39:08 +0300

    added NativeLoader

--- tabulator/loaders/__init__.py
+++ tabulator/loaders/__init__.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 from .api import Loader as API
 from .file import FileLoader as File
+from .native import NativeLoader as Native
 from .stream import StreamLoader as Stream
 from .text import TextLoader as Text
 from .web import WebLoader as Web
---
+++ tabulator/loaders/native.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+
+from .. import errors, helpers
+from . import api
+
+
+# Module API
+
+class NativeLoader(api.Loader):
+    """Null loader to pass python native lists.
+    """
+
+    # Public
+
+    def __init__(self, source, encoding=None, **options):
+        self.__source = source
+        self.__encoding = encoding
+        self.__options = options
+
+    def load(self, mode):
+        raise errors.Error('NativeLoader doesn\'t support load method')
+
+    @property
+    def source(self):
+        return self.__source
+
+    @property
+    def encoding(self):
+        return self.__encoding
