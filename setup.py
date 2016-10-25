fcbb0ef23748ebe0e7a2f090a2006a3a61362880
roll <roll@post.agency>
2016-10-25 12:09:00 +0300

    relax under v1 deps

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ INSTALL_REQUIRES = [
     'requests>=2.8,<3.0a',
     'beautifulsoup4>=4.4,<5.0a',
     'linear-tsv>=1.0,<2.0a',
-    'unicodecsv>=0.14,<0.15a',
+    'unicodecsv>=0.14,<1.0a',
 ]
 TESTS_REQUIRE = [
     'pylama',
