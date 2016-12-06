e99f7470e953dd5f6455b3811d469b622a081093
roll <eskarev@gmail.com>
2016-12-06 14:17:54 +0300

    added detect_zip test for ods

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -65,6 +65,11 @@ def test_detect_zip_false():
     assert helpers.detect_zip(sample) == False
 
 
+def test_detect_zip_ods():
+    sample = io.open('data/table.ods', mode='rb').read(config.BYTES_SAMPLE_SIZE)
+    assert helpers.detect_zip(sample) == True
+
+
 def test_detect_zip_xlsx():
     sample = io.open('data/table.xlsx', mode='rb').read(config.BYTES_SAMPLE_SIZE)
     assert helpers.detect_zip(sample) == True
