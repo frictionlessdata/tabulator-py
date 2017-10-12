a77de1a7f44ebed2256fb0b95833c6a82086d427
roll <roll@users.noreply.github.com>
2017-10-12 11:02:29 +0300

    Updated to datapackage-v1 (#205)
    * fix: resource_iter as a list of dictionaries
    
    * Updated to datapackage@1.0

--- tabulator/parsers/datapackage.py
+++ tabulator/parsers/datapackage.py
@@ -4,18 +4,16 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import datapackage
 import six
-
+import datapackage
 from ..parser import Parser
+from .. import exceptions
 
 
 # Module API
 
 class DataPackageParser(Parser):
     """Parser to extract data from Tabular Data Packages.
-
-    See: http://specs.frictionlessdata.io/
     """
 
     # Public
@@ -26,39 +24,37 @@ class DataPackageParser(Parser):
 
     def __init__(self, loader, force_parse=False, resource=0):
         self.__force_parse = force_parse
-        self.__resource = resource
+        self.__resource_pointer = resource
         self.__extended_rows = None
-        self.__datapackage = None
-        self.__resource_iter = None
         self.__encoding = None
+        self.__resource = None
 
     @property
     def closed(self):
-        return self.__resource_iter is None
+        return self.__extended_rows is None
 
     def open(self, source, encoding=None):
         self.close()
-        self.__datapackage = datapackage.DataPackage(source)
+        package = datapackage.DataPackage(source)
+        if isinstance(self.__resource_pointer, six.string_types):
+            self.__resource = package.get_resource(self.__resource_pointer)
+        else:
+            try:
+                self.__resource = package.resources[self.__resource_pointer]
+            except (TypeError, IndexError):
+                pass
+        if not self.__resource:
+            message = 'Data package "%s" doesn\'t have resource "%s"'
+            raise exceptions.SourceError(message % (source, self.__resource_pointer))
+        self.__resource.infer()
+        self.__encoding = self.__resource.descriptor.get('encoding')
         self.reset()
 
     def close(self):
         if not self.closed:
-            self.__datapackage = None
-            self.__resource_iter = None
             self.__extended_rows = None
 
     def reset(self):
-        if isinstance(self.__resource, six.string_types):
-            named_resource = next(iter(filter(
-                lambda res: res.descriptor['name'] == self.__resource,
-                self.__datapackage.resources
-            )))  # TODO: use data_package.getResource(name) when v1 is released
-            self.__resource_iter = named_resource.iter()
-            self.__encoding = named_resource.descriptor.get('encoding')
-        else:
-            indexed_resource = self.__datapackage.resources[self.__resource]
-            self.__resource_iter = indexed_resource.iter()
-            self.__encoding = indexed_resource.descriptor.get('encoding')
         self.__extended_rows = self.__iter_extended_rows()
 
     @property
@@ -72,6 +68,5 @@ class DataPackageParser(Parser):
     # Private
 
     def __iter_extended_rows(self):
-        for number, row in enumerate(self.__resource_iter, start=1):
-            keys, values = zip(*sorted(row.items()))
-            yield number, list(keys), list(values)
+        for row_number, headers, row in self.__resource.iter(extended=True):
+            yield (row_number - 1, headers, row)
