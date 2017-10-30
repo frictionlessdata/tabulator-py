5a7a154136dce95392bfd8e9c0c82aefad20fedf
roll <eskarev@gmail.com>
2017-10-30 14:15:04 +0300

    Fixed csv dialect bug

--- tabulator/parsers/csv.py
+++ tabulator/parsers/csv.py
@@ -119,7 +119,7 @@ class CSVParser(Parser):
             if not dialect.escapechar:
                 dialect.doublequote = True
         except csv.Error:
-            dialect = csv.excel
+            class dialect(csv.excel): pass
         for key, value in self.__options.items():
             setattr(dialect, key, value)
 
