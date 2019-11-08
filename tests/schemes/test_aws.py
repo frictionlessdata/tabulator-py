470cb2ddc7efd62a6c073c88339f37f7fd1c6c54
roll <eskarev@gmail.com>
2019-11-08 14:23:04 +0300

    Skip aws test on py2

--- tests/schemes/test_aws.py
+++ tests/schemes/test_aws.py
@@ -20,6 +20,7 @@ S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL'] = 'http://localhost:5000'
 
 # Stream
 
+@pytest.mark.skipif(six.PY2, reason='Support only for Python3')
 def test_stream_s3(s3_client, bucket):
 
     # Upload a file
@@ -35,6 +36,7 @@ def test_stream_s3(s3_client, bucket):
         assert stream.read() == [['id', 'name'], ['1', 'english'], ['2', '中国人']]
 
 
+@pytest.mark.skipif(six.PY2, reason='Support only for Python3')
 def test_stream_s3_endpoint_url(s3_client, bucket):
 
     # Upload a file
