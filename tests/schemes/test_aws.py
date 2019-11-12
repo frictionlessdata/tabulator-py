ca5581a1dd3ceb5a9a0c1e6add585bb88c7d56ae
roll <eskarev@gmail.com>
2019-11-12 14:14:22 +0300

    Fixed aws tests

--- tests/schemes/test_aws.py
+++ tests/schemes/test_aws.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 import os
 import six
+import sys
 import boto3
 import pytest
 import string
@@ -21,7 +22,7 @@ S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL'] = 'http://localhost:5000'
 
 # Stream
 
-@pytest.mark.skipif(six.PY2, reason='Support only for Python3')
+@pytest.mark.skipif(sys.version_info[:2] != (3, 7), reason='Cannot test in parallel')
 def test_stream_s3(s3_client, bucket):
 
     # Upload a file
@@ -37,7 +38,7 @@ def test_stream_s3(s3_client, bucket):
         assert stream.read() == [['id', 'name'], ['1', 'english'], ['2', '中国人']]
 
 
-@pytest.mark.skipif(six.PY2, reason='Support only for Python3')
+@pytest.mark.skipif(sys.version_info[:2] != (3, 7), reason='Cannot test in parallel')
 def test_stream_s3_endpoint_url(s3_client, bucket):
 
     # Upload a file
