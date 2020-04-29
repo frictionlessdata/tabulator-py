9d04d6a8667c62b04d5455b5770d97df54a987a2
roll <eskarev@gmail.com>
2020-04-29 09:39:04 +0300

    Implemented the writer

--- tabulator/config.py
+++ tabulator/config.py
@@ -62,6 +62,7 @@ PARSERS = {
 
 WRITERS = {
     'csv': 'tabulator.writers.csv.CSVWriter',
+    'json': 'tabulator.writers.csv.JSONWriter',
     'xlsx': 'tabulator.writers.xlsx.XLSXWriter',
     'sql': 'tabulator.writers.sql.SQLWriter',
 }
