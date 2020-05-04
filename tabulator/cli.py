35f70b1d289736ae4d28941f9446a04f63d3e1ea
roll <roll@users.noreply.github.com>
2020-05-04 11:43:20 +0300

    Exposed more options to CLI (#319)
    * Exposed more options to CLI
    
    * Fixed the implementation

--- tabulator/cli.py
+++ tabulator/cli.py
@@ -8,6 +8,7 @@ import six
 import click
 import tabulator
 from . import config
+from . import exceptions
 
 
 # Module API
@@ -19,6 +20,15 @@ from . import config
 @click.option('--format')
 @click.option('--encoding')
 @click.option('--limit', type=click.INT)
+@click.option('--sheet')
+@click.option('--fill-merged-cells', is_flag=True, default=None)
+@click.option('--preserve-formatting', is_flag=True, default=None)
+@click.option('--adjust-floating-point-error', is_flag=True, default=None)
+@click.option('--table')
+@click.option('--order_by')
+@click.option('--resource')
+@click.option('--property')
+@click.option('--keyed', is_flag=True, default=None)
 @click.version_option(config.VERSION, message='%(version)s')
 def cli(source, limit, **options):
     """Command-line interface
@@ -32,19 +42,41 @@ def cli(source, limit, **options):
       --format TEXT
       --encoding TEXT
       --limit INTEGER
+      --sheet TEXT/INTEGER (excel)
+      --fill-merged-cells BOOLEAN (excel)
+      --preserve-formatting BOOLEAN (excel)
+      --adjust-floating-point-error BOOLEAN (excel)
+      --table TEXT (sql)
+      --order_by TEXT (sql)
+      --resource TEXT/INTEGER (datapackage)
+      --property TEXT (json)
+      --keyed BOOLEAN (json)
       --version          Show the version and exit.
       --help             Show this message and exit.
     ```
 
     """
+
+    # Normalize options
     options = {key: value for key, value in options.items() if value is not None}
-    with tabulator.Stream(source, **options) as stream:
-        cast = str
-        if six.PY2:
-            cast = unicode  # noqa
-        if stream.headers:
-            click.echo(click.style(', '.join(map(cast, stream.headers)), bold=True))
-        for count, row in enumerate(stream, start=1):
-            click.echo(','.join(map(cast, row)))
-            if count == limit:
-                break
+    try:
+        options['sheet'] = int(options.get('sheet'))
+        options['resource'] = int(options.get('resource'))
+    except Exception:
+        pass
+
+    # Read the table
+    try:
+        with tabulator.Stream(source, **options) as stream:
+            cast = str
+            if six.PY2:
+                cast = unicode  # noqa
+            if stream.headers:
+                click.echo(click.style(', '.join(map(cast, stream.headers)), bold=True))
+            for count, row in enumerate(stream, start=1):
+                click.echo(','.join(map(cast, row)))
+                if count == limit:
+                    break
+    except exceptions.TabulatorException as exception:
+        click.echo('[error] %s' % str(exception))
+        exit(1)
